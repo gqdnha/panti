7a292560172d2ffebd5a4d2789059ce0604f37b1
gqdnha <2726254524@qq.com>
2025-05-08 20:35:41 +0800

    优化

--- pages/userPages/oneWrongBook_type/ontWrongBook_type.js
+++ pages/userPages/oneWrongBook_type/ontWrongBook_type.js
@@ -1,14 +1,14 @@
 import {
-    getOneWrongQuestion,
-    apiJudgeWrongQuestion
-} from "../../../api/getWrongQuestion";
-
+    getWrongQuestion,
+} from "../../api/getWrongQuestion";
+import {addLearnTime} from '../../api/addLearnTime'
+import {apiJudgeTest} from '../../api/judgeTest'
 Page({
     data: {
         userId:0,
-        type:'',
+        type: '',
         //  查看详情
-        detailData:{},
+        detailData: {},
 
         // 页面数据
         currentQuestion: 1, //题目序号
@@ -29,7 +29,7 @@ Page({
         this.setData({
             type,
             userId
-        })
+        });
         console.log(type);
         console.log(userId);
         this.getData();
@@ -52,25 +52,32 @@ Page({
             res.forEach(question => {
                 if (question.options && typeof question.options === 'string') {
                     try {
-                        question.options = JSON.parse(question.options)
+                        question.options = JSON.parse(question.options);
                     } catch (error) {
                         console.log('选项解析错误：', error);
-                        question.options = []
+                        question.options = [];
                     }
                 }
-            })
+            });
+            // 初始化选中选项数组和选项状态数组
+            const initialSelectedOptions = new Array(res.length).fill(null).map(() => []);
+            const initialOptionStates = res.map(question =>
+                new Array(question.options.length).fill(false)
+            );
+
             this.setData({
                 allQuestions: res,
                 totalQuestions: res.length,
                 questionStates: new Array(res.length).fill(null), // 初始化题目状态数组
-                selectedOptions: new Array(res.length).fill(null), // 初始化选中选项数组
-                isSubmitted: new Array(res.length).fill(false) // 初始化题目提交状态数组
-            })
+                selectedOptions: initialSelectedOptions,
+                optionStates: initialOptionStates,
+                answerSheetStates: new Array(res.length).fill(false)
+            });
             console.log(this.data.allQuestions);
             console.log(this.data.totalQuestions);
             console.log(this.data.allQuestions[1].options);
 
-        })
+        });
     },
     nextQuestion: function () {
         const { currentQuestion, totalQuestions, selectedOptions } = this.data;
@@ -103,42 +110,36 @@ Page({
     // 多选题
     selectMultipleOption: function (e) {
         const { index } = e.currentTarget.dataset;
-        const { currentQuestion, selectedOptions, allQuestions } = this.data;
+        const { currentQuestion, selectedOptions, allQuestions, optionStates } = this.data;
 
-        // 确保 currentQuestion 在有效范围内
-        if (currentQuestion < 1 || currentQuestion > this.data.totalQuestions) {
-            return;
-        }
+        // 获取当前题目的选项状态
+        let currentOptionStates = [...optionStates[currentQuestion - 1]];
+        // 切换当前选项的状态
+        currentOptionStates[index] = !currentOptionStates[index];
 
-        const newSelectedOptions = [...selectedOptions];
-        const optionFirstChar = allQuestions[currentQuestion - 1].options[index][0];
-        if (!Array.isArray(newSelectedOptions[currentQuestion - 1])) {
-            newSelectedOptions[currentQuestion - 1] = [optionFirstChar];
-        } else {
-            const currentSelected = newSelectedOptions[currentQuestion - 1];
-            const optionIndex = currentSelected.indexOf(optionFirstChar);
-            if (optionIndex > -1) {
-                currentSelected.splice(optionIndex, 1);
-            } else {
-                currentSelected.push(optionFirstChar);
+        // 更新选中选项
+        let currentSelected = [];
+        currentOptionStates.forEach((isSelected, idx) => {
+            if (isSelected) {
+                currentSelected.push(allQuestions[currentQuestion - 1].options[idx][0]);
             }
-        }
-
-        // 打印更新前的 selectedOptions
-        console.log('更新前的 selectedOptions:', this.data.selectedOptions);
-
-        this.setData({
-            selectedOptions: newSelectedOptions
-        }, () => {
-            // 打印更新后的 selectedOptions
-            console.log('更新后的 selectedOptions:', this.data.selectedOptions);
-            // 检查视图层是否接收到更新后的数据
-            const updatedSelectedOptions = this.data.selectedOptions;
-            console.log('视图层接收到的 updatedSelectedOptions:', updatedSelectedOptions);
         });
 
-        const selectedChars = newSelectedOptions[currentQuestion - 1] || [];
-        console.log(`第 ${currentQuestion} 题选中的选项首字：`, selectedChars);
+        // 对选项进行排序
+        currentSelected.sort();
+
+        // 更新数据
+        const newSelectedOptions = [...selectedOptions];
+        newSelectedOptions[currentQuestion - 1] = currentSelected;
+
+        const newOptionStates = [...optionStates];
+        newOptionStates[currentQuestion - 1] = currentOptionStates;
+
+        this.setData({
+            selectedOptions: newSelectedOptions,
+            optionStates: newOptionStates,
+            [`answerSheetStates[${currentQuestion - 1}]`]: true
+        });
     },
     onInputAnswer: function (e) {
         const { value } = e.detail;
@@ -149,14 +150,14 @@ Page({
             allAnswers: newAllAnswers
         });
     },
-    /* submitSingleAnswer: function () {
+    submitSingleAnswer: function () {
         const { allQuestions, allAnswers, selectedOptions, questionStates, currentQuestion } = this.data;
         const newQuestionStates = [...questionStates];
         const newIsSubmitted = [...this.data.isSubmitted];
         const question = allQuestions[currentQuestion - 1];
         const userAnswer = allAnswers[currentQuestion - 1];
         let userAnswerToSubmit;
-    
+
         if (question.type === '单选题' || question.type === '判断题') {
             const selectedChar = selectedOptions[currentQuestion - 1];
             userAnswerToSubmit = selectedChar || '';
@@ -168,18 +169,20 @@ Page({
         } else if (question.type === '填空题') {
             userAnswerToSubmit = userAnswer;
         }
-    
+
         newIsSubmitted[currentQuestion - 1] = true;
-    
+
         this.setData({
             isSubmitted: newIsSubmitted
         });
-    
-        apiJudgeWrongQuestion([{
-            'questionId': question.questionId,
-            'answer': userAnswerToSubmit
-        }])
-           .then(response => {
+        const data = [{
+            questionId: question.questionId,
+            answer: userAnswerToSubmit,
+            type:this.data.type
+        }]
+        console.log(data);
+
+        apiJudgeTest(data).then(response => {
                 console.log('后端返回结果：', response);
                 const result = response[0];
                 const isCorrect = result.rightOrWrong === '对';
@@ -195,23 +198,49 @@ Page({
            .catch(error => {
                 console.error('提交答案到后端时出错：', error);
             });
-    }, */
+    },
 
-    /* showAnalysis: function () {
+    showAnalysis: function () {
         const { currentQuestion, allQuestions } = this.data;
         const currentQuestionData = allQuestions[currentQuestion - 1];
         this.setData({
             showAnalysis: true,
             currentQuestionData: currentQuestionData
         });
-    }, */
-    /* closeAnalysisAndContinue: function () {
+    },
+    closeAnalysisAndContinue: function () {
         this.setData({
             showAnalysis: false
         });
-    }, */
+    },
     // 自定义函数，用于判断数组是否包含某个元素
-    /* isArrayAndIncludes: function (arr, item) {
+    isArrayAndIncludes: function (arr, item) {
         return Array.isArray(arr) && arr.includes(item);
-    } */
-})    
+    },
+    onUnload: function () {
+        const { startTime } = this.data;
+        const endTime = new Date();
+        const durationInMinutes = Math.floor((endTime - startTime) / (1000 * 60));
+        console.log(`做题总时间（分钟）：${durationInMinutes}`);
+        addLearnTime(durationInMinutes).then(res => {
+            console.log(res);
+        })
+    },
+    submitAnswer() {
+        if (this.data.isSubmitted) return;
+        
+        const currentQuestion = this.data.currentQuestion - 1;
+        const question = this.data.allQuestions[currentQuestion];
+        const options = question.options;
+        
+        // 判断答案是否正确
+        options.forEach(option => {
+            option.isCorrect = option.selected === option.isAnswer;
+        });
+        
+        this.setData({
+            isSubmitted: true,
+            [`allQuestions[${currentQuestion}].options`]: options
+        });
+    }
+});
